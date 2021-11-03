626030839c558e7ba2670f4eecaf65ab54043aa7
niharreddy1 <niharreddy1@outlook.com>
2021-11-03 18:59:15 -0500

    Finished Landscapes

--- src/Images/SiteImages.js
+++ src/Images/SiteImages.js
@@ -28,6 +28,28 @@ const SiteImages = {
 
   //Landscapes Main Photo
   landscapesMain: require("../Images/Landscapes/MainLandscapesImg.jpg").default,
+  landscapesOne: require("../Images/Landscapes/DSC_0008(1).jpg").default,
+  landscapesTwo: require("../Images/Landscapes/DSC_0024.jpg").default,
+  landscapesThree: require("../Images/Landscapes/DSC_0029.jpg").default,
+  landscapesFour: require("../Images/Landscapes/DSC_0033.jpg").default,
+  landscapesFive: require("../Images/Landscapes/DSC_0160.jpg").default,
+  landscapesSix: require("../Images/Landscapes/DSC_0233.jpg").default,
+  landscapesSeven: require("../Images/Landscapes/DSC_0270.jpg").default,
+  landscapesEight: require("../Images/Landscapes/DSC_0394.jpg").default,
+  landscapesNine: require("../Images/Landscapes/DSC_0434.jpg").default,
+  landscapesTen: require("../Images/Landscapes/DSC_0525.jpg").default,
+  landscapesEleven: require("../Images/Landscapes/DSC_0548.jpg").default,
+  landscapesTwelve: require("../Images/Landscapes/DSC_0648 2.jpg").default,
+  landscapesThirteen: require("../Images/Landscapes/DSC_0822.jpg").default,
+  landscapesFourteen: require("../Images/Landscapes/DSC_0823.jpg").default,
+  landscapesFifteen: require("../Images/Landscapes/DSC_0995.jpg").default,
+  landscapesSixteen: require("../Images/Landscapes/DSC_1163.jpeg").default,
+  landscapesSeventeen: require("../Images/Landscapes/DSC_1163.jpg").default,
+  landscapesEighteen: require("../Images/Landscapes/IMG_3959.jpeg").default,
+  landscapesNineteen:
+    require("../Images/Landscapes/Photo_6553730_DJI_130_jpg_4317209_0_2021411380_photo_original.jpeg")
+      .default,
+  landscapesTwenty: require("../Images/Landscapes/IMG_4357.jpeg").default,
 
   // Portraits Main Photo
   portraitsMain: require("../Images/Portraits/MainPortraitsImg.jpg").default,
--- src/pages/LandscapesPage.js
+++ src/pages/LandscapesPage.js
@@ -9,7 +9,149 @@ const LandscapesPage = () => {
     <div>
       <SiteSectionHeader coverImg={SiteImages.landscapesMain} />
 
-      <div className="pt-28 pb-12"></div>
+      <div className="pt-28 pb-12">
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesOne}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesOne"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesTwo}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesTwo"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesThree}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesThree"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesFour}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFour"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesFive}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesSix}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesSeven}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesEight}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesNine}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesTen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesEleven}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesTwelve}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesThirteen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesFourteen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesFifteen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesSixteen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesSeventeen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesEighteen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+        <div class="flex flex-wrap justify-center space-x-10 pb-32">
+          <img
+            src={SiteImages.landscapesNineteen}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesFive"
+            class="shadow-xl rounded"
+          />
+          <img
+            src={SiteImages.landscapesTwenty}
+            style={{ height: "45%", width: "45%" }}
+            alt="landscapesSix"
+            class="shadow-xl rounded"
+          />
+        </div>
+      </div>
 
       <Footer />
     </div>
